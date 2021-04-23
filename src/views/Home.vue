6259069b74183afab45bcdc2c98bf9f9f14f475e
graylogo <graylogo@gmail.com>
2021-04-23 15:57:11 +0800

    数组练习

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="home">
-    严金刚的修改
+    <el-tree
+      show-checkbox
+      ref="tree"
+      class="tree"
+      :data="data"
+      :props="defaultProps"
+      @node-click="handleNodeClick"
+      @check-change="getCheckedNodes"
+    ></el-tree>
   </div>
 </template>
 
@@ -9,6 +17,100 @@
 
 export default {
   name: "Home",
-  components: {}
+  components: {},
+  data() {
+    return {
+      data: [
+        {
+          label: "一级 1",
+          children: [
+            {
+              label: "二级 1-1",
+              children: [
+                {
+                  label: "三级 1-1-1"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          label: "一级 2",
+          children: [
+            {
+              label: "二级 2-1",
+              children: [
+                {
+                  label: "三级 2-1-1"
+                }
+              ]
+            },
+            {
+              label: "二级 2-2",
+              children: [
+                {
+                  label: "三级 2-2-1"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          label: "一级 3",
+          children: [
+            {
+              label: "二级 3-1",
+              children: [
+                {
+                  label: "三级 3-1-1"
+                }
+              ]
+            },
+            {
+              label: "二级 3-2",
+              children: [
+                {
+                  label: "三级 3-2-1"
+                },
+                {
+                  label: "三级 3-2-2"
+                }
+              ]
+            },
+            {
+              label: "二级 3-3",
+              children: [
+                {
+                  label: "三级 3-3-1",
+                  disabled: true
+                }
+              ]
+            }
+          ]
+        }
+      ],
+      defaultProps: {
+        children: "children",
+        label: "label",
+        disabled: "disableed"
+      }
+    };
+  },
+  methods: {
+    handleNodeClick(data) {
+      console.log(data);
+    },
+    handleCheckChange(data, checked, indeterminate) {
+      console.log(data, checked, indeterminate, "aaaa");
+    },
+    getCheckedNodes() {
+      console.log(this.$refs.tree.getCheckedNodes(true), "bbbb");
+    }
+  }
 };
 </script>
+<style scoped>
+.tree {
+  width: 600px;
+}
+</style>
