36bbd15edf546ae32b92dae39fac47ae39ff8e32
graylogo <graylogo@gmail.com>
2021-05-28 17:03:14 +0800

    前端配置跨域代理

--- vue.config.js
+++ vue.config.js
@@ -4,12 +4,36 @@
 module.exports = {
   configureWebpack: {
     devServer: {
+      host: 'localhost',
       port: 8888, // 端口号
-      open: true
+      open: true,
+      https: false,
       // Mock数据
       // before(app){
 
       // }
+      //   跨域配置
+      //   proxy: { // 配置跨域
+      //     '/api': {
+      //       target: 'http://localhost:8008/api/', // 这里后台的地址模拟的;应该填写你们真实的后台接口
+      //       ws: true,
+      //       changOrigin: true, // 允许跨域
+      //       pathRewrite: {
+      //         '^/api': ''// 请求的时候使用这个api就可以
+      //       }
+      //     }
+      // }
+      proxy: { // 配置跨域
+        '/api': {
+          target: 'http://localhost:8008/api/', // 这里后台的地址模拟的;应该填写你们真实的后台接口
+          ws: true,
+          changOrigin: true, // 允许跨域
+          pathRewrite: {
+            '^/api': ''// 请求的时候使用这个api就可以
+          }
+        }
+
+      }
     }
   }
 }
